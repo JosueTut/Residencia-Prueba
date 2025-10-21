ae054cc329b7d62e81c22a0d3f4b75e6fc2f13bb
Josue Tut <josuetut33@gmail.com>
2025-10-21 15:55:39 -0500

    add of divs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const handleClick = () => {
+    window.open('https://github.com/JosueTut/Residencia-Prueba', '_blank');
+  }
 
   return (
     <>
       <h1>Vite + React</h1>
       <div className="bg-stone-950 text-stone-50 w-full sm:bg-blue-500 sm:w-sm md:bg-green-500 md:w-md lg:bg-yellow-500 xl:bg-red-500 2xl:bg-purple-500">
         Hola Mundo
+      </div>
+      <div className='h-3/4 flex flex-col items-center justify-center'>
+        <div onClick={handleClick} className='bg-blue-200 w-3/4 size-32 p-6 my-8 text-center items-center justify-center flex rounded-lg border-1 border-blue-400 hover:bg-blue-300 cursor-pointer'>
+          Edificio 1
+        </div>
+
+        <div onClick={handleClick} className='bg-blue-200 w-3/4 size-32 p-6 my-8 text-center items-center justify-center flex rounded-lg border-1 border-blue-400 hover:bg-blue-300 cursor-pointer'>
+          Edificio 2
+        </div>
+
+        <div onClick={handleClick} className='bg-blue-200 w-3/4 size-32 p-6 my-8 text-center items-center justify-center flex rounded-lg border-1 border-blue-400 hover:bg-blue-300 cursor-pointer'>
+          Edificio 3
+        </div>
+
+        <div onClick={handleClick} className='bg-blue-200 w-3/4 size-32 p-6 my-8 text-center items-center justify-center flex rounded-lg border-1 border-blue-400 hover:bg-blue-300 cursor-pointer'>
+          Edificio 4
+        </div>
+
+        <div onClick={handleClick} className='bg-blue-200 w-3/4 size-32 p-6 my-8 text-center items-center justify-center flex rounded-lg border-1 border-blue-400 hover:bg-blue-300 cursor-pointer'>
+          Edificio 5
+        </div>
+
+        <div onClick={handleClick} className='bg-blue-200 w-3/4 size-32 p-6 my-8 text-center items-center justify-center flex rounded-lg border-1 border-blue-400 hover:bg-blue-300 cursor-pointer'>
+          Edificio 6
+        </div>
       </div>
     </>
   )
